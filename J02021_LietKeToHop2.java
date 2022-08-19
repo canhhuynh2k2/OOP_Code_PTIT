41e8f6872bd37bf41edaa626f612d615c5e5d6ef
Nguyễn Cảnh Huỳnh <canhhuynh2002@gmail.com>
2022-08-19 15:51:29 +0700

    LIỆT KÊ TỔ HỢP - 2

---
+++ J02021_LietKeToHop2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class J02021_LietKeToHop2 {
+	static Scanner in = new Scanner(System.in);
+	static int n, k, a[] = new int[15], cnt = 0;
+	static void Try(int i) {
+		for(int j = a[i-1] + 1; j <= n - k + i; j++) {
+			a[i] = j;
+			if(i == k) {
+				for(int d = 1; d <= k; d++) System.out.print(a[d]);
+				System.out.print(" ");
+				cnt++;
+			}
+			else Try(i+1);
+		}
+	}
+	public static void main(String[] args) {
+		n = in.nextInt();
+		k = in.nextInt();
+		Try(1);
+		System.out.println("\nTong cong co " + cnt + " to hop");
+	}
+
+}
