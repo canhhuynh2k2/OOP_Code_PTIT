76a5b17b518f2b26b4da7f74324516a729f1c373
Nguyễn Cảnh Huỳnh <canhhuynh2002@gmail.com>
2022-08-24 13:54:07 +0700

    ĐỌC FILE VĂN BẢN

---
+++ J07001_DocFileVanBan.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class J07001_DocFileVanBan {
+    public static void main(String[] args) {
+        try {
+            File myObj = new File("DATA.in");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+              String data = myReader.nextLine();
+              System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
