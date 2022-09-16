0563b1264a7c5467a0420be9559c37386fdfacea
Nguyễn Cảnh Huỳnh <canhhuynh2002@gmail.com>
2022-09-16 22:36:10 +0700

    GÕ BÀN PHÍM

---
+++ JKT015_GoBanPhim.java
@@ -0,0 +1,36 @@
+/* @author canhhuynh2k2 */
+import java.util.*;
+public class JKT015_GoBanPhim {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        ArrayDeque<Character> a = new ArrayDeque<Character>();
+        ArrayDeque<Character> b = new ArrayDeque<Character>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '<'){
+                if(a.size() > 0){
+                    b.push(a.pop());
+                }
+            }
+            else if(s.charAt(i) == '>'){
+                if(b.size() > 0){
+                    a.push(b.pop());
+                }
+            }
+            else if(s.charAt(i) == '-'){
+                if(a.size() > 0){
+                    a.pop();
+                }
+            }
+            else{
+                a.push(s.charAt(i));
+            }
+        }
+        while(a.size() > 0){
+            b.push(a.pop());
+        }
+        while(b.size() > 0){
+            System.out.printf("%c", b.pop());
+        }
+    }
+}
